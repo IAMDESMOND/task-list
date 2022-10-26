2dfe17044b7849dac6c2fc9accdda79976f838a5
IAMDESMOND <emmades247@gmail.com>
2022-10-26 22:04:46 +0100

    added files

---
+++ app.js
@@ -0,0 +1,8 @@
+//variables
+
+const taskForm = document.querySelector(".task_form");
+const taskInput = document.querySelector("#task");
+const addBtn = document.querySelector(".add_btn");
+const taskList = document.querySelector(".task_list");
+const filterInput = document.querySelector("#filter");
+const clearBtn = document.querySelector(".clear_btn")
