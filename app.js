8c8ff0b35efe66fb22b3ed0b8fdda5172f81ddb8
IAMDESMOND <emmades247@gmail.com>
2022-10-27 20:16:39 +0100

    added list through javasript

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 //variables
 
-const taskForm = document.querySelector(".task_form");
-const taskInput = document.querySelector("#task");
-const addBtn = document.querySelector(".add_btn");
-const taskList = document.querySelector(".task_list");
-const filterInput = document.querySelector("#filter");
-const clearBtn = document.querySelector(".clear_btn")
+const taskForm = document.querySelector('.task_form');
+const taskInput = document.querySelector('#task');
+const addBtn = document.querySelector('.add_btn');
+const taskList = document.querySelector('.task_list');
+const filterInput = document.querySelector('#filter');
+const clearBtn = document.querySelector('.clear_btn');
+
+// load all eventlistener
+
+loadAllEventlisteners();
+
+function loadAllEventlisteners () {
+    taskForm.addEventListener('submit', addTask)
+}
+
+//add task function
+
+function addTask (e) {
+    if (taskInput.value === '') {
+        alert('Add a task');
+    }else{
+    //create li element
+    const li = document.createElement('li');
+    li.className = 'task_list-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+    
+    //create link
+    const link = document.createElement('a');
+    link.className = 'remove_btn';
+    link.innerHTML = '<i class="fa fa-remove delete_btn"></i>';
+    li.appendChild(link)
+    taskList.appendChild(li);
+    taskInput.value = '';
+    }
+    e.preventDefault();
+}
